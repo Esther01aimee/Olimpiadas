ef5de4fb74601ba54255800b75c03782ff1c2d0f
Esther Aimee Santos Souza <estherass.itb@gmail.com>
2024-09-07 20:41:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, conhecida como a Fadinha, é um fenômeno do skate mundial. Com apenas [idade da Rayssa], ela já conquistou diversos títulos e se tornou um ícone para as novas gerações de skatistas. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+      link: "https://en.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate sk8 skatista fadinha medalha rayssa"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma destacada judoca brasileira. Com sua técnica apurada e determinação, ela conquistou diversos títulos nacionais e internacionais, representando o Brasil em grandes competições. Sua história de superação e dedicação a inspirou muitos jovens atletas.",
+      link: "https://en.wikipedia.org/wiki/Beatriz_Souza",
+      tags:"judo judô judoka medalha ouro beatriz"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua alegria e talento. Em sua participação nas Olimpíadas de Tóquio, ela se tornou a primeira ginasta brasileira a conquistar uma medalha de ouro individual, um marco histórico para o esporte feminino no Brasil.",
+      link: "https://en.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginasta ginastica ginástica medalha ouro rebeca"
+    }
+  ];
